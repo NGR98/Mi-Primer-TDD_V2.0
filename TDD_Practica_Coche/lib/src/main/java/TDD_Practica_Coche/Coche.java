a47167209f704b0ba65f489803f18ac26149915a
Nicolas <NicoGilRuiz@gmail.com>
2023-04-02 13:46:31 +0200

    1º Commit

---
+++ TDD_Practica_Coche/lib/src/main/java/TDD_Practica_Coche/Coche.java
@@ -0,0 +1,14 @@
+package TDD_Practica_Coche;
+
+public class Coche {
+	public Integer velocidad = 0;
+
+	public void acelerar(int aceleracion) {
+		velocidad += aceleracion;		
+	}
+
+	public void decelerar(int deceleracion) {
+		velocidad -= deceleracion;
+		if (velocidad < 0) velocidad = 0;
+	}
+}
